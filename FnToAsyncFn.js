d4b40a68bdbd441397a718b87daa2abd3fe24143
Marcio Lage <52936273+tangoins@users.noreply.github.com>
2021-05-25 11:08:51 -0300

    Create FnToAsyncFn.js

---
+++ FnToAsyncFn.js
@@ -0,0 +1,13 @@
+//  Wrap a funtion with a trusted promise making that function Async
+
+function sum(v1, v2){
+  return(v1 + v2);
+}
+
+function promisify(fn){
+  let args = [].slice.call(arguments);
+
+  return Promise.resolve(fn.apply(null,args.slice(1)));
+}
+
+promisify(sum,20,40).then(result => console.log(result));
