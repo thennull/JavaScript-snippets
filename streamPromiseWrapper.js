cb05fefbe89c81f7ad0c890a7d6bd0695d01e161
Marcio Lage <thennull.dev@gmail.com>
2021-06-28 08:10:52 -0300

    Update streamPromiseWrapper.js

--- streamPromiseWrapper.js
+++ streamPromiseWrapper.js
@@ -1,9 +1,8 @@
 function streamWrapper(stream){
   return new Promise(function(res,rej){
     stream.on("end", function(){
-      your logic goes here!!
         res();
-      or
+      // Just a template....
         rej();
       });
     });
